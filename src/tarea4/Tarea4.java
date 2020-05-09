473798b42ae3a9e61c3c0981f501a8cb9abb1dc2
jalvarezbretana <jalvarezbretana@danielcastelao.org>
2020-05-09 16:14:58 +0200

    Tarefa 4 rematada

---
+++ src/tarea4/Tarea4.java
@@ -0,0 +1,21 @@
+package tarea4;
+
+/**
+ *
+ * @author josel
+ */
+public class Tarea4 {
+
+    public static void main(String[] args) {
+    Ventana vent = new Ventana();
+    vent.setVisible(true);
+    /*
+    Agregar - Añade una fila con los datos de la GUI y limpia los campos
+    Modificar - Modifica los datos de la fila cambiandolos a los actuales en los cuadros superiores
+    Borrar - Borra la ultima fila
+    
+    */
+    
+    }
+    
+}
